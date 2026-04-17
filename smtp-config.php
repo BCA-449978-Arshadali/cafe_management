22783e7fe7b14e5e8fc958e0c564aff47ae23779
BCA-449978-Arshadali <arshad.bcastudent23.9978@cimage.in>
2026-04-17 18:17:48 +0530

    Update smtp-config.php

--- smtp-config.php
+++ smtp-config.php
@@ -4,8 +4,8 @@
 
 define('SMTP_HOST',       'smtp.gmail.com');
 define('SMTP_PORT',       587);
-define('SMTP_USERNAME',   'dropperscafe.auth@gmail.com');  // Apna Gmail
-define('SMTP_PASSWORD',   'wrkofzhynacmwjil');     // 
+define('SMTP_USERNAME', getenv('SMTP_USERNAME'));
+define('SMTP_PASSWORD', getenv('SMTP_PASSWORD'));     // 
 define('SMTP_FROM_EMAIL', 'dropperscafe.auth@gmail.com');
 define('SMTP_FROM_NAME',  'Droppers Cafe');
 
